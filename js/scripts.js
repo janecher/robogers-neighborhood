72172f8195f1b1c12a8e8a5a1dad10d8e890fe2f
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-12 09:40:16 -0700

    add functionality to scripts.js to get, check and output user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
+//buisness logic
+const convertToArray = function(number){
+  let numberArray =[];
+  for(let i=1; i<=number; i++){
+    numberArray.push(i);
+  }
+  return numberArray;
+}
 
+//user interface logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    const number = parseInt($("input#number").val());
+    if(!number || number < 1){
+      alert("Incorrect input, your input should be a positive number");
+      $("input#number").val("")
+      return;
+    }
+    const arrayOfNumbers = convertToArray(number);
+    arrayOfNumbers.forEach(function(element){
+      $("ol#result").append("<li>"+element+"</li>")
+    });
+    $(".result").show();
+  });
 });
